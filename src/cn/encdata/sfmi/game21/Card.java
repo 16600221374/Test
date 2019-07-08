29fbfd14e805687152c12a687a1cea4776f1e1b0
single <2766033991@qq.com>
2019-07-08 13:07:27 +0800

    111222

---
+++ src/cn/encdata/sfmi/game21/Card.java
@@ -0,0 +1,21 @@
+package cn.encdata.sfmi.game21;
+
+public class Card {
+	public int type,value;
+
+    public int getType() {
+        return type;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
